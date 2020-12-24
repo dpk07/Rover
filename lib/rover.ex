c4db236b9539c6aa52c95d9fa760c2357ca85709
Deepak Pai <paidpk07@gmail.com>
2020-12-24 18:02:19 +0530

    Refactored the code for Rover

--- lib/rover.ex
+++ lib/rover.ex
@@ -29,39 +29,47 @@ defmodule Rover do
   @doc """
   Moves the rover in the provided direction for provided steps.
   ## Examples
-      iex> Rover.init()
-      %Rover{direction: "N", x: 0, y: 0}
-
-      iex> Rover.move(rover,"L",0)
-      %Rover{direction: "W", x: 0, y: 0}
+      iex> Rover.init() |> Rover.move("L",0) |> Rover.move("R",1)
+      %Rover{direction: "N", x: 0, y: 1}
 
   """
   def move(rover, direction, steps) do
     do_move(rover, direction, steps)
   end
 
-  defp do_move(rover, @left, steps) do
-    case rover.direction do
-      @north -> update(rover, @west, rover.x - steps, rover.y)
-      @south -> update(rover, @east, rover.x + steps, rover.y)
-      @east -> update(rover, @north, rover.x, rover.y + steps)
-      @west -> update(rover, @south, rover.x, rover.y - steps)
-    end
+  defp do_move(rover = %Rover{direction: @west, x: x, y: y}, @left, steps) do
+    update(rover, @south, x, y - steps)
   end
 
-  defp do_move(rover, @right, steps) do
-    case rover.direction do
-      @north -> update(rover, @east, rover.x + steps, rover.y)
-      @south -> update(rover, @west, rover.x - steps, rover.y)
-      @east -> update(rover, @south, rover.x, rover.y - steps)
-      @west -> update(rover, @north, rover.x, rover.y + steps)
-    end
+  defp do_move(rover = %Rover{direction: @east, x: x, y: y}, @left, steps) do
+    update(rover, @north, x, y + steps)
+  end
+
+  defp do_move(rover = %Rover{direction: @north, x: x, y: y}, @left, steps) do
+    update(rover, @west, x - steps, y)
+  end
+
+  defp do_move(rover = %Rover{direction: @south, x: x, y: y}, @left, steps) do
+    update(rover, @east, x + steps, y)
+  end
+
+  defp do_move(rover = %Rover{direction: @west, x: x, y: y}, @right, steps) do
+    update(rover, @north, x, y + steps)
+  end
+
+  defp do_move(rover = %Rover{direction: @east, x: x, y: y}, @right, steps) do
+    update(rover, @south, x, y - steps)
+  end
+
+  defp do_move(rover = %Rover{direction: @north, x: x, y: y}, @right, steps) do
+    update(rover, @east, x + steps, y)
+  end
+
+  defp do_move(rover = %Rover{direction: @south, x: x, y: y}, @right, steps) do
+    update(rover, @west, x - steps, y)
   end
 
   defp update(rover, direction, x, y) do
-    rover
-    |> Map.update!(:direction, fn _ -> direction end)
-    |> Map.update!(:x, fn _ -> x end)
-    |> Map.update!(:y, fn _ -> y end)
+    %Rover{rover | direction: direction, x: x, y: y}
   end
 end
--- test/rover_test.exs
+++ test/rover_test.exs
@@ -2,7 +2,22 @@ defmodule RoverTest do
   use ExUnit.Case
   doctest Rover
 
-  test "greets the world" do
-    assert Rover.hello() == :world
+  test "Creating a rover" do
+    rover = Rover.init()
+    assert rover.direction == "N"
+    assert rover.x == 0
+    assert rover.y == 0
+  end
+
+  test "Moving a new rover" do
+    rover = Rover.init()
+    rover = Rover.move(rover, "L", 0)
+    assert rover.direction == "W"
+    assert rover.x == 0
+    assert rover.y == 0
+    rover = Rover.move(rover, "R", 1)
+    assert rover.direction == "N"
+    assert rover.x == 0
+    assert rover.y == 1
   end
 end
